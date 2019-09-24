b692fb6d46ce68d2dc3436b8b5fcd4679821b470
Arne Kvaleberg <arnekva@gmail.com>
2019-09-24 12:07:47 +0200

    window.onload()

--- WebContent/js/GuiHandler.js
+++ WebContent/js/GuiHandler.js
@@ -2,65 +2,61 @@
 
 class GuiHandler {
 	
-	showTask(task){
-		
-		if (document.getElementById("taskTab") == null){
-			var taskDiv = document.getElementById("tasks");
-			console.log("step1");
-			var tabell = document.createElement("table");
-			tabell.setAttribute("id", "taskTab");
-			var thead = document.createElement("thead");
-			tabell.appendChild(thead);
-				var headRow = document.createElement("tr");
-				thead.appendChild(headRow);
-				var th1 = document.createElement("th");
-				var thTxt1 = document.createTextNode("Task");
-				th1.appendChild(thTxt1);
-				headRow.appendChild(th1);
-				var th2 = document.createElement("th");
-				var thTxt2 = document.createTextNode("Status");
-				th2.appendChild(thTxt2);
-				headRow.appendChild(th2);
-			var tbody = document.createElement("tbody");
-			tbody.setAttribute("id", "tableBody");
-			tabell.appendChild(tbody);
-		}
-		console.log("step2");
-		var tbody2 = document.getElementById("tableBody");
-			var tRow = document.createElement("tr");
-			tRow.setAttribute("id", task.id);
-			tbody2.appendChild(tRow);
-				var td1 = document.createElement("td");
-				var tdTxt1 = document.createTextNode(task.title);
-				td1.appendChild(tdTxt1);
-				tRow.appendChild(td1);
-				var td2 = document.createElement("td");
-				var tdTxt2 = document.createTextNode(task.status);
-				td2.appendChild(tdTxt2);
-				tRow.appendChild(td2);
-				
+	constructor() {
+		this.table = document.getElementById('maintable');
 	}
-	
+
+	addRow(task) {
+		let row = this.table.insertRow(-1);
+
+		row.insertCell(0).innerHTML = task.id;
+		row.insertCell(1).innerHTML = task.title;
+		row.insertCell(2).innerHTML = task.status;
+	}
+
+	editRow(e){
+		let row = e.parenNode.parenNode.rowIndex;
+	}
+
+	deleteRow(i) {
+		this.table.deleteRow(i);
+	}
 	
 }
 
 
+const gui = new GuiHandler()
 
-function skalKallesIBegynelsen() {
+window.onload = function()
+{setupStatus()};
+document.getElementById("newtaskbtn").onclick = function()
+{addNewTask()};
+
+function addNewTask(newtask){
+	gui.addRow(newtask);
+}
+function updateTask(newtask){
+	gui.editrow(newtask);
+}
+
+function setupStatus() {
+	
 	console.log("step3");
-	const gui = new GuiHandler()
+	
 	const statuses = ["WAITING","ACTIVE","DONE"]
 	const tasks = [
-	    {"id":1,"title":"Paint roof","status":"WAITING"},
-	    {"id":2,"title":"Clean floor","status":"DONE"},
-	    {"id":3,"title":"Wash windows","status":"ACTIVE"}
+	    {id:1,title:"Paint roof",status:"WAITING"},
+	    {id:2,title:"Clean floor",status:"DONE"},
+	    {id:3,title:"Wash windows",status:"ACTIVE"}
 	]
-
-	gui.allstatuses = statuses;
-	tasks.forEach((task) => {gui.showTask(task)})
+ for(let i =0; i<tasks.length;i++){
+	 gui.addRow(tasks[i]);
+ }
+	
 }
 
 function testAvHtmlInsert() {
+	
 	var endreDiv = document.getElementById("tasks");
 	var nyNode = document.createElement("p");
 	nyNode.setAttribute("id", "idTest");
