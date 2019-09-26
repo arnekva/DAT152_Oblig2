0297f6ed7c3300e78b57fbebfda42002d0cf73c8
sivertlunde <571734@stud.hvl.no>
2019-09-26 10:13:28 +0200

    fjernet unødvendige metoder

--- WebContent/js/GuiHandler.js
+++ WebContent/js/GuiHandler.js
@@ -122,37 +122,3 @@ function setupStatus() {
  }
 
 }
-
-
-function testAvHtmlInsert() {
-
-	var endreDiv = document.getElementById("tasks");
-	var nyNode = document.createElement("p");
-	nyNode.setAttribute("id", "idTest");
-	var txt = document.createTextNode("Tester insertion av html-noder");
-	nyNode.appendChild(txt);
-	endreDiv.appendChild(nyNode);
-	var idHenteTest = document.getElementById("idTest");
-	var nyNode2 = document.createElement("p");
-	var txt2 = document.createTextNode("Tester id på ny noder");
-	nyNode2.appendChild(txt2);
-	idHenteTest.appendChild(nyNode2);
-
-}
-
-function gjemmerMidlertidigVekk() {
-	this.allstatuses.forEach(status => console.log(status));
-	console.log(task.id);
-
-
-
-	var td3 = document.createElement("td");
-	var select = document.createElement("select");
-	td3.appendChild(select);
-		var option = document.createElement("option");
-		option.setAttribute("value", "0");
-		option.setAttribute("selected", " ");
-		var oTxt1 = document.createTextNode("&lt;");
-td2.appendChild(tdTxt2);
-tRow.appendChild(td2);
-}
