724e2db3fbb4cc38265fc4716094d387c884b2ae
sivertlunde <571734@stud.hvl.no>
2019-09-27 13:57:59 +0200

    la til nødvendige js filer

---
+++ WebContent/js/delete.js
@@ -0,0 +1,18 @@
+"use strict";
+
+async function main() {
+    const url='../TaskServices/broker/task/2'
+    try {
+        const response = await fetch(url,{method: "DELETE"})
+        try {
+            const text = await response.text()
+            console.log(text)
+        } catch (error) {
+            console.log(error)
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+ 
+main()
