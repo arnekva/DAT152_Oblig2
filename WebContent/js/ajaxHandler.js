6840be454f4db9857631705b57a91fdc02d206bd
sivertlunde <571734@stud.hvl.no>
2019-09-27 14:34:10 +0200

    Laget ajaxHandler med alle nødvendige scripts mot database

---
+++ WebContent/js/ajaxHandler.js
@@ -0,0 +1,90 @@
+"use strict";
+
+class ajaxHandler {
+	
+	async allstatuses() {
+	    const url='../TaskServices/broker/allstatuses'
+
+	    try {
+	        const response = await fetch(url,{method: "GET"})
+	        try {
+	            const text = await response.text()
+	            console.log(text)
+	        } catch (error) {
+	            console.log(error)
+	            }
+	        } catch (error) {
+	            console.log(error)
+	        }
+	}
+	
+	async deleteTask() {
+	    const url='../TaskServices/broker/task/2'
+	    try {
+	        const response = await fetch(url,{method: "DELETE"})
+	        try {
+	            const text = await response.text()
+	            console.log(text)
+	        } catch (error) {
+	            console.log(error)
+	        }
+	    } catch (error) {
+	        console.log(error)
+	    }
+	}
+	
+	async modifyStatus() {
+	    const url='../TaskServices/broker/task/2'
+	    try {
+	        const response = await fetch(url,{
+	            method: "PUT",
+	            headers: {"Content-Type": "application/json; charset=utf-8"},
+	            body: JSON.stringify({'status': 'DONE'})
+	        })
+	        try {
+	            const text = await response.text()
+	            console.log(text)
+	        } catch (error) {
+	            console.log(error)
+	        }
+	    } catch (error) {
+	        console.log(error)
+	    }
+	}
+	
+	async addNewTask() {
+	    
+	    const url='../TaskServices/broker/task'
+	    try {
+	        const response = await fetch(url,{
+	            method: "POST",
+	            headers: {"Content-Type": "application/json; charset=utf-8"},
+	            body: JSON.stringify({"title":"Something more to do", "status": "WAITING"})
+	        })
+	        try {
+	            const text = await response.text()
+	            console.log(text)
+	        } catch (error) {
+	            console.log(error)
+	        }
+	    } catch (error) {
+	        console.log(error)
+	    }
+	}
+	
+	async getAllTasks() {
+	    const url='../TaskServices/broker/tasklist'
+	    try {
+	        const response = await fetch(url,{method: "GET"})
+	        try {
+	            const text = await response.text()
+	            console.log(text)
+	        } catch (error) {
+	            console.log(error)
+	        }            
+	    } catch (error) {
+	        console.log(error)
+	    }
+	}
+	
+}
