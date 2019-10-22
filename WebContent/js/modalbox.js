10dc5c7b0aea066ec238bca60fbb9d77958dc3d2
Arne Kvaleberg <arnekva@gmail.com>
2019-10-22 13:14:53 +0200

    JSDoc for modalbox

--- WebContent/js/modalbox.js
+++ WebContent/js/modalbox.js
@@ -1,5 +1,10 @@
 class ModalBox{
-
+	
+	/**
+	 * @param {List} allstatuses
+	 * @param {Function} onsubmit
+	 * 			
+	 */
 	constructor(){
 		
 		this.onsubmitCallbacks = new Array()
@@ -13,15 +18,21 @@ class ModalBox{
 		btn.addEventListener("click", () => this.submit(), true)
 		span.addEventListener("click", () => this.close(), true)
 	}
-	
+	/**
+	 * Sets the callback to be called on submit
+	 */
 	set onsubmitCallback(method){
 		this.onsubmitCallbacks.push(method)
 	}
-	
+	/**
+	 * All statuses setter
+	 */
 	set allstatuses(statuses){
 		this._allstatuses = statuses
 	}
-	
+	 /**
+	 * Function displaying the modal box
+	 */
 	show(){
 		
 		let modal = document.getElementById("myModal")
@@ -38,7 +49,9 @@ class ModalBox{
 		}
 		modal.style.display = "block";
 	}
-	
+	 /**
+	 * Function that fetches data from input fields and creates a JSON structure Task object for the submit
+	 */
 	submit(){
 		let titletxt = document.getElementById("tasktext").value;
 		let statuslist = document.getElementById("modalstatuslist");
@@ -53,7 +66,9 @@ class ModalBox{
 		document.getElementById("tasktext").value = "";
 		
 	}
-	
+	 /**
+	 * Closes the modal box
+	 */
 	close(){
 		let box = document.getElementById("myModal")
 		box.style.display = "none"
